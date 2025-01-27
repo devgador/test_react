172ff491567dc96bc3ccaaaf6bf2777697446a46
mijaso <lbm90210@gmail.org>
2025-01-27 00:51:33 -0400

    deplo2

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename= '/stages-jobs-steps'>
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
